734eac7e2f99f45e7be76156a3dd51c43833c75f
ALI <ali11salem00@gmail.com>
2022-07-24 02:08:36 +0300

    first commit

---
+++ app/src/main/java/com/example/clockapp/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.clockapp
+
+import android.os.Binder
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModelProvider
+import com.example.clockapp.databinding.ActivityMainBinding
+import com.google.android.material.card.MaterialCardView
+
+class MainActivity : AppCompatActivity() {
+    private val viewModel:MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bindLiveData(viewModel.segmentTopLiveData,binding.segmentTop.root)
+        bindLiveData(viewModel.segmentTopLeftLiveData,binding.segmentTopLeft.root)
+        bindLiveData(viewModel.segmentTopRightLiveData,binding.segmentTopRight.root)
+        bindLiveData(viewModel.segmentMiddleLiveData,binding.segmentMiddle.root)
+        bindLiveData(viewModel.segmentBottomLiveData,binding.segmentBottom.root)
+        bindLiveData(viewModel.segmentBottomLeftLiveData,binding.segmentBottomLeft.root)
+        bindLiveData(viewModel.segmentBottomRightLiveData,binding.segmentBottomRight.root)
+
+        viewModel.startCounting()
+    }
+    private fun bindLiveData(liveData:LiveData<Int>,materialCardView: MaterialCardView){
+        liveData.observe(this){colorRes ->
+            materialCardView.apply {
+                val resolvedColor = ContextCompat.getColor(context,colorRes)
+                setBackgroundColor(resolvedColor)
+            }
+        }
+    }
+}
